6b2e5ed9da397a5ce4c513a728a138b95a97680e
Ben Hymers <b.hymers@gmail.com>
2009-03-24 16:31:53 +0000

    OfxEditor project added; currently just a viewer for OFX files. OfxCommon project added; aims to be a common base for all OFX-based stuff. It includes a trimmed-down OFX schema and a C# class generated from that, for other projects to deserialise the XML parts of OFX files into.

---
+++ OfxEditor/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace OfxEditor
+{
+    partial class OfxEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.accountIDTextBox = new System.Windows.Forms.TextBox();
+            this.bankIDTextBox = new System.Windows.Forms.TextBox();
+            this.statementStartDateTextBox = new System.Windows.Forms.TextBox();
+            this.statementEndDateTextBox = new System.Windows.Forms.TextBox();
+            this.ledgerBalanceTextBox = new System.Windows.Forms.TextBox();
+            this.accountTypeLabel = new System.Windows.Forms.Label();
+            this.accountIDLabel = new System.Windows.Forms.Label();
+            this.bankIDLabel = new System.Windows.Forms.Label();
+            this.statementStartDateLabel = new System.Windows.Forms.Label();
+            this.statementEndDateLabel = new System.Windows.Forms.Label();
+            this.ledgerBalanceLabel = new System.Windows.Forms.Label();
+            this.accountTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.ledgerBalanceAsOfTextBox = new System.Windows.Forms.TextBox();
+            this.ledgerBalanceAsOfLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 91);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 603);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // accountIDTextBox
+            // 
+            this.accountIDTextBox.Location = new System.Drawing.Point(88, 39);
+            this.accountIDTextBox.Name = "accountIDTextBox";
+            this.accountIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.accountIDTextBox.TabIndex = 3;
+            // 
+            // bankIDTextBox
+            // 
+            this.bankIDTextBox.Location = new System.Drawing.Point(88, 65);
+            this.bankIDTextBox.Name = "bankIDTextBox";
+            this.bankIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.bankIDTextBox.TabIndex = 4;
+            // 
+            // statementStartDateTextBox
+            // 
+            this.statementStartDateTextBox.Location = new System.Drawing.Point(358, 12);
+            this.statementStartDateTextBox.Name = "statementStartDateTextBox";
+            this.statementStartDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.statementStartDateTextBox.TabIndex = 5;
+            // 
+            // statementEndDateTextBox
+            // 
+            this.statementEndDateTextBox.Location = new System.Drawing.Point(358, 38);
+            this.statementEndDateTextBox.Name = "statementEndDateTextBox";
+            this.statementEndDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.statementEndDateTextBox.TabIndex = 6;
+            // 
+            // ledgerBalanceTextBox
+            // 
+            this.ledgerBalanceTextBox.Location = new System.Drawing.Point(592, 12);
+            this.ledgerBalanceTextBox.Name = "ledgerBalanceTextBox";
+            this.ledgerBalanceTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ledgerBalanceTextBox.TabIndex = 7;
+            // 
+            // accountTypeLabel
+            // 
+            this.accountTypeLabel.AutoSize = true;
+            this.accountTypeLabel.Location = new System.Drawing.Point(12, 15);
+            this.accountTypeLabel.Name = "accountTypeLabel";
+            this.accountTypeLabel.Size = new System.Drawing.Size(70, 13);
+            this.accountTypeLabel.TabIndex = 8;
+            this.accountTypeLabel.Text = "Account type";
+            // 
+            // accountIDLabel
+            // 
+            this.accountIDLabel.AutoSize = true;
+            this.accountIDLabel.Location = new System.Drawing.Point(21, 42);
+            this.accountIDLabel.Name = "accountIDLabel";
+            this.accountIDLabel.Size = new System.Drawing.Size(61, 13);
+            this.accountIDLabel.TabIndex = 9;
+            this.accountIDLabel.Text = "Account ID";
+            // 
+            // bankIDLabel
+            // 
+            this.bankIDLabel.AutoSize = true;
+            this.bankIDLabel.Location = new System.Drawing.Point(36, 68);
+            this.bankIDLabel.Name = "bankIDLabel";
+            this.bankIDLabel.Size = new System.Drawing.Size(46, 13);
+            this.bankIDLabel.TabIndex = 10;
+            this.bankIDLabel.Text = "Bank ID";
+            // 
+            // statementStartDateLabel
+            // 
+            this.statementStartDateLabel.AutoSize = true;
+            this.statementStartDateLabel.Location = new System.Drawing.Point(250, 15);
+            this.statementStartDateLabel.Name = "statementStartDateLabel";
+            this.statementStartDateLabel.Size = new System.Drawing.Size(102, 13);
+            this.statementStartDateLabel.TabIndex = 11;
+            this.statementStartDateLabel.Text = "Statement start date";
+            // 
+            // statementEndDateLabel
+            // 
+            this.statementEndDateLabel.AutoSize = true;
+            this.statementEndDateLabel.Location = new System.Drawing.Point(252, 41);
+            this.statementEndDateLabel.Name = "statementEndDateLabel";
+            this.statementEndDateLabel.Size = new System.Drawing.Size(100, 13);
+            this.statementEndDateLabel.TabIndex = 12;
+            this.statementEndDateLabel.Text = "Statement end date";
+            // 
+            // ledgerBalanceLabel
+            // 
+            this.ledgerBalanceLabel.AutoSize = true;
+            this.ledgerBalanceLabel.Location = new System.Drawing.Point(505, 15);
+            this.ledgerBalanceLabel.Name = "ledgerBalanceLabel";
+            this.ledgerBalanceLabel.Size = new System.Drawing.Size(81, 13);
+            this.ledgerBalanceLabel.TabIndex = 13;
+            this.ledgerBalanceLabel.Text = "Ledger balance";
+            // 
+            // accountTypeComboBox
+            // 
+            this.accountTypeComboBox.FormattingEnabled = true;
+            this.accountTypeComboBox.Location = new System.Drawing.Point(88, 12);
+            this.accountTypeComboBox.Name = "accountTypeComboBox";
+            this.accountTypeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.accountTypeComboBox.TabIndex = 14;
+            // 
+            // ledgerBalanceAsOfTextBox
+            // 
+            this.ledgerBalanceAsOfTextBox.Location = new System.Drawing.Point(592, 38);
+            this.ledgerBalanceAsOfTextBox.Name = "ledgerBalanceAsOfTextBox";
+            this.ledgerBalanceAsOfTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ledgerBalanceAsOfTextBox.TabIndex = 15;
+            // 
+            // ledgerBalanceAsOfLabel
+            // 
+            this.ledgerBalanceAsOfLabel.AutoSize = true;
+            this.ledgerBalanceAsOfLabel.Location = new System.Drawing.Point(543, 41);
+            this.ledgerBalanceAsOfLabel.Name = "ledgerBalanceAsOfLabel";
+            this.ledgerBalanceAsOfLabel.Size = new System.Drawing.Size(43, 13);
+            this.ledgerBalanceAsOfLabel.TabIndex = 16;
+            this.ledgerBalanceAsOfLabel.Text = "... As of";
+            // 
+            // OfxEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 706);
+            this.Controls.Add(this.ledgerBalanceAsOfLabel);
+            this.Controls.Add(this.ledgerBalanceAsOfTextBox);
+            this.Controls.Add(this.accountTypeComboBox);
+            this.Controls.Add(this.ledgerBalanceLabel);
+            this.Controls.Add(this.statementEndDateLabel);
+            this.Controls.Add(this.statementStartDateLabel);
+            this.Controls.Add(this.bankIDLabel);
+            this.Controls.Add(this.accountIDLabel);
+            this.Controls.Add(this.accountTypeLabel);
+            this.Controls.Add(this.ledgerBalanceTextBox);
+            this.Controls.Add(this.statementEndDateTextBox);
+            this.Controls.Add(this.statementStartDateTextBox);
+            this.Controls.Add(this.bankIDTextBox);
+            this.Controls.Add(this.accountIDTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "OfxEditor";
+            this.Text = "OfxEditor";
+            this.Load += new System.EventHandler(this.OfxEditor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox accountIDTextBox;
+        private System.Windows.Forms.TextBox bankIDTextBox;
+        private System.Windows.Forms.TextBox statementStartDateTextBox;
+        private System.Windows.Forms.TextBox statementEndDateTextBox;
+        private System.Windows.Forms.TextBox ledgerBalanceTextBox;
+        private System.Windows.Forms.Label accountTypeLabel;
+        private System.Windows.Forms.Label accountIDLabel;
+        private System.Windows.Forms.Label bankIDLabel;
+        private System.Windows.Forms.Label statementStartDateLabel;
+        private System.Windows.Forms.Label statementEndDateLabel;
+        private System.Windows.Forms.Label ledgerBalanceLabel;
+        private System.Windows.Forms.ComboBox accountTypeComboBox;
+        private System.Windows.Forms.TextBox ledgerBalanceAsOfTextBox;
+        private System.Windows.Forms.Label ledgerBalanceAsOfLabel;
+    }
+}
+
---
+++ OfxEditor/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace OfxEditor
+{
+    public partial class OfxEditor : Form
+    {
+        private string fileName;
+
+        public OfxEditor(string fileName)
+        {
+            this.fileName = fileName;
+            InitializeComponent();
+        }
+
+        private void OfxEditor_Load(object sender, EventArgs e)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(OFX.OFX));
+            TextReader reader = new StreamReader(fileName);
+            OFX.OFX ofx = (OFX.OFX)serializer.Deserialize(reader);
+            reader.Close();
+
+            dataGridView1.DataSource = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN;
+
+            accountTypeComboBox.DataSource = System.Enum.GetNames(typeof(OFX.OFXBANKMSGSRSV1STMTTRNRSSTMTRSBANKACCTFROMACCTTYPE));
+            accountTypeComboBox.SelectedItem = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKACCTFROM.ACCTTYPE;
+            accountIDTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKACCTFROM.ACCTID;
+            bankIDTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKACCTFROM.BANKID;
+            statementStartDateTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTSTART;
+            statementEndDateTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.DTEND;
+            ledgerBalanceTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.BALAMT.ToString();
+            ledgerBalanceAsOfTextBox.Text = ofx.BANKMSGSRSV1.STMTTRNRS.STMTRS.LEDGERBAL.DTASOF;
+
+        }
+    }
+}
